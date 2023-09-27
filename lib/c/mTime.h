2c9b9ecd8fac9640f208db1693d88c159f6fc4eb
Harry Han (Yuhao) <harryhan912@gmail.com>
2023-09-27 23:04:29 +0100

    journalctl

--- lib/c/mTime.h
+++ lib/c/mTime.h
@@ -4,16 +4,42 @@
 #ifndef _MTIME_H
 #define _MTIME_H
 
+#include <sys/time.h>
 #include <time.h>
-#include <stdint.h>
-#include <inttypes.h>
+#include <errno.h>
 
+/// Current time in microseconds since epoch
 
-/* Current time in Nanoseconds since epoch*/
-void mTime(long * in){
-	struct timespec spec;
-	clock_gettime(CLOCK_REALTIME, &spec);
-	*in = spec.tv_sec*1000000000L + spec.tv_nsec;
-	return;
+// Complaint to C99 
+
+long int mTime(long *in){
+  long current_calendar_time = 0;
+  // timeval defined in sys/time.h
+  struct timeval tp; 
+  gettimeofday(&tp, NULL);
+  
+  current_calendar_time += tp.tv_sec*(long)(1000000) + tp.tv_usec;
+  *in = current_calendar_time;
+	return current_calendar_time;
 }
+
+// Input: long micro_sec
+// the current thread sleeps for micro_sec microseconds
+// if interrupted by system signals during the waiting time,
+// the remaining waiting time is simply ignored
+int microSleep(long micro_sec){
+  if (micro_sec < 0) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  struct timespec ts;
+  ts.tv_sec = micro_sec/1000000;
+  ts.tv_nsec = (micro_sec%1000) * 1000;
+
+  int res = nanosleep(&ts, NULL);
+
+  return res;
+}
+
 #endif // endif for _MTIME_H
